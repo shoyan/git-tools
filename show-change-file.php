79982b9df809f83fb4234ac56266585a8c9f6067
Shohei Yamasaki <shyamasaki@gmail.com>
2012-09-25 10:18:36 +0900

    change filename

---
+++ show-change-file.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * 指定されたコミットから変更されたファイル一覧を作成します 
+ *
+ * [注意点]
+ * 見たいコミットの1個前を指定してください
+ * 例えば以下のような履歴があったとします。
+ *
+ * commit02
+ * commit01
+ * commit00
+ *
+ * commit01からの変更が見たい場合は、commit00を指定してください
+ * commit01を指定すると、commit02からの変更しか見れません。
+ * これは git logが指定された次のコミットから表示するようになっているからです。
+ *
+ * SYNOPSIS
+ * php show-change-file.php [commit]
+ * php show-change-file.php [tagname]
+ *
+ * EXAMPLE
+ * php show-change-file.php 36316..
+ * php show-change-file.php v1.0..
+ */
+
+$command = "git log --name-status " . $argv[1] . "..";
+$content = shell_exec($command);
+
+$filename = 'show-change-file.txt';
+
+if (!$handle = fopen($filename, 'w+')) {
+    echo "Cannot open file ($filename)";
+    exit;
+}
+
+if (fwrite($handle, $content) === FALSE) {
+    echo "Cannot write to file ($filename)";
+    exit;
+}
+
+
+fclose($handle);
+
+$stack = array();
+$lines = file('show-change-file.txt');
+
+foreach ($lines as $line_num => $string) {
+
+    $pattern = '/commit .+/';
+    if (preg_match($pattern, $string)) {
+        $string = '';
+    }
+
+    $pattern = '/Author:.+/i';
+    if (preg_match($pattern, $string)) {
+        $string = '';
+    }
+
+    $pattern = '/Date:.+/i';
+    if (preg_match($pattern, $string)) {
+        $string = '';
+    }
+
+    $pattern = '/    .+/i';
+    if (preg_match($pattern, $string)) {
+        $string = '';
+    }
+
+    $pattern = '/Merge:.+/i';
+    if (preg_match($pattern, $string)) {
+        $string = '';
+    }
+
+    if (!empty($string)) {
+        $stack[] = $string;
+    }
+}
+sort($stack);
+$stack = array_unique($stack);
+foreach ($stack as $line) {
+    echo $line;
+}
+
+unlink('show-change-file.txt');
+
+?>
